cabf88ef5b0a16610784c33128ed720e0ba2cfac
Pasha Feldman <pasha74.dev@gmail.com>
2025-07-14 11:02:25 +0300

    supabase server, middleware, login page initial

---
+++ src/app/login/page.tsx
@@ -0,0 +1,22 @@
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import AuthButton from "@/components/auth/AuthButton";
+
+export default async function LoginPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/");
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <AuthButton />
+    </div>
+  );
+}
