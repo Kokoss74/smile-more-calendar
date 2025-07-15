b0f608b87b3dba967d46a5f9ad0fccbb9150d489
Pasha Feldman <pasha74.dev@gmail.com>
2025-07-16 02:18:51 +0300

    feature PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const pwaConfig = {
+  dest: "public",
+};
+
+export default withPWA(pwaConfig)(nextConfig);
